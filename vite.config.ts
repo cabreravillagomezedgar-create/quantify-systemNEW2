4747d76569ccb7c456ed6acd811a090fb12e7c1a
cabreravillagomezedgar-create <cabreravillagomezedgar@gmail.com>
2025-11-07 01:06:33 -0600

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
+import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 export default defineConfig({
-  root: ".",
+  plugins: [react()],
+  root: "client",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"),
+    },
+  },
   build: {
-    outDir: "dist",
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
 });
